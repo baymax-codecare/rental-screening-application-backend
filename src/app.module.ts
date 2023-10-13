2d9db6a5acee2af9c790d8f9a99aa7181fc0b817
baymax <pony.kadeem@gmail.com>
2023-10-13 02:15:34 -0600

    feat: sendgrid service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { PublicFileModule } from './public-file/public-file.module';
+import { SendgridService } from './sendgrid/sendgrid.service';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { PublicFileModule } from './public-file/public-file.module';
     PublicFileModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SendgridService],
 })
 export class AppModule {}
