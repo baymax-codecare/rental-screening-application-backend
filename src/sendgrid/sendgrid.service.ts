5b96662afd3498a8a451f2d628e2f7c6d43f0568
baymax <pony.kadeem@gmail.com>
2023-10-13 02:15:34 -0600

    feat: sendgrid service

---
+++ src/sendgrid/sendgrid.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as SendGrid from '@sendgrid/mail';
+
+@Injectable()
+export class SendgridService {
+  constructor(private readonly configService: ConfigService) {
+    SendGrid.setApiKey(this.configService.get<string>('sendgrid.apiKey'));
+  }
+
+  async send(mail: SendGrid.MailDataRequired) {
+    const transport = await SendGrid.send(mail);
+
+    return transport;
+  }
+}
